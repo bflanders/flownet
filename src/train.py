84a3d868117898428885fcb3ca22aa4147dc2ff4
bflanders <benjaminjflanders@gmail.com>
2019-10-05 21:41:34 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,17 @@
+"""train.py
+
+This provides a CLI for training a neural network for image recognition.
+
+Usage:
+    >> python train.py -s . -d ./flowers -g -e 20
+    * -s : save checkpoint 
+    * -d ./flowers : location of the data directory
+    * -g : use GPU
+    * -e 20 : use 20 epochs
+
+"""
 #%% Imports
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from datetime import datetime
 import numpy as np
 from pathlib import Path
@@ -11,7 +23,9 @@ from torchvision import datasets, models, transforms
 from time import time as timer
 
 #%% Parse arguments
-parser = ArgumentParser(description='Train model on data set')
+parser = ArgumentParser(description=__doc__,
+    formatter_class=RawDescriptionHelpFormatter
+)
 # Architecture
 arch_map = {
     'alexnet': models.alexnet
@@ -52,25 +66,25 @@ add = parser.add_argument
 # Positional
 
 add('-a', '--arch', type=str, default='vgg16_default'
-    ,help='Architecture: Pick model from torch.models (default: vgg16)')
+    ,help='Architecture: Pick model from torch.models')
 # Checkpoint
 add('-c', '--checkpoint', type=str, default=None
     ,help='Optional checkpoint file')
 add('-d', '--data_dir', action='store', help='Data directory (must have "train", "test", and "valid" subdirectories)')
 # Epochs
-add('-e', '--epochs', type=int, default='-1', help='Number of epochs (default: 20)')
+add('-e', '--epochs', type=int, default='-1', help='Number of epochs')
 # GPU
-add('-g', '--gpu', action='store_true', help='Use GPU if available (default: False)?')
+add('-g', '--gpu', action='store_true', help='Use GPU if available')
 # Learning rate
-add('-r','--learning_rate', type=float, default='-1'
-    ,help='Learning rate (default: 0.01)')
+add('-r','--learning_rate', type=float, default=-1.0
+    ,help='Learning rate')
 # Save directory
 add('-s', '--save_dir', type=str, required=True
     ,help='Save: path to save directory (**required**)')
 # Hidden units
 add('-u', '--hidden_units', metavar='N'
-    ,help='List of hidden layers (default: 512 256)'
-    ,default=[-1, 512, 256])
+    ,help='List of hidden layers'
+    ,default=[-1])
 args = parser.parse_args()
 
 #%% Class definitions
@@ -78,8 +92,9 @@ class Model:
     def __init__(self):
         # Architecture
         if args.arch=='vgg16_default':
-            print('WARN: No architecture (-a, --arch) specified. Using arch = VGG16')
             self.arch = 'vgg16'
+            print('WARN: No architecture (-a, --arch) specified. Using arch = '+str(self.arch))
+            
         else:
             err_msg = '\n\nSelect an architecture (-a, --arch) from the following choices: '
             if args.arch in arch_map:
@@ -88,8 +103,8 @@ class Model:
                 raise ValueError(err_msg+', '.join(arch_map.keys()))
         # Hidden layers
         if args.hidden_units[0]==-1:
-            self.hidden_units = [512, 256]
-            print('WARN: No hidden units specified (-l, --hidden_units). Using [512, 256]')
+            self.hidden_units = [4096, 256]
+            print('WARN: No hidden units specified (-l, --hidden_units). Using '+str(self.hidden_units))
         else:
             self.hidden_units = args.hidden_units
         # GPU
@@ -97,13 +112,13 @@ class Model:
         # Epochs
         if args.epochs==-1:
             self.epochs = 20
-            print('WARN: No epochs (-e, --epochs) specified. Using epochs = 20')
+            print('WARN: No epochs (-e, --epochs) specified. Using epochs = '+str(self.epochs))
         else:
             self.epochs = abs(args.epochs)
         # Learning rate
-        if args.learning_rate==-1:
-            self.lr = 0.01
-            print('WARN: No learning rate (-l, --learning_rate) specified. Using learning_rate = 0.01')
+        if args.learning_rate==-1.0:
+            self.lr = 0.001
+            print('WARN: No learning rate (-l, --learning_rate) specified. Using learning_rate = '+str(self.lr))
         else:
             self.lr = abs(args.learning_rate)
         # Data directory
@@ -171,10 +186,11 @@ class Model:
         cadd("rel0", torch.nn.ReLU())
         cadd("dp0", torch.nn.Dropout(drop_rate))
         hu = self.hidden_units # alias
-        for i,d in enumerate(hu[:-1]):
-            cadd("hl"+str(i+1), torch.nn.Linear(hu[i], hu[i+1]))
-            cadd("rel"+str(i+1), torch.nn.ReLU())
-            cadd("dp"+str(i+1), torch.nn.Dropout(drop_rate))
+        if len(self.hidden_units) > 1:
+            for i,d in enumerate(hu[:-1]):
+                cadd("hl"+str(i+1), torch.nn.Linear(hu[i], hu[i+1]))
+                cadd("rel"+str(i+1), torch.nn.ReLU())
+                cadd("dp"+str(i+1), torch.nn.Dropout(drop_rate))
         cadd("final", torch.nn.Linear(hu[-1], num_output))
         cadd("output", torch.nn.LogSoftmax(dim=1))
 
@@ -186,33 +202,31 @@ class Model:
         optimizer = optim_func(self.model.classifier.parameters(), self.lr)
         start = timer()
         device = self.device # alias
-        model = self.model   # alias
-        model.to(device)
+        self.model.to(device)
         for e in range(self.epochs):
             train_loss, test_loss, accuracy = 0, 0, 0
             # Train
             print("Epoch: {}/{}".format(e+1, self.epochs), end=' steps.. ', flush=True)
-            model.train()
-            torch.enable_grad()
+            self.model.train()
             for images, labels in self.data_loaders['train']:
                 images, labels = images.to(device), labels.to(device)
                 optimizer.zero_grad()
-                outputs = model.forward(images)
+                outputs = self.model.forward(images)
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
                 train_loss += loss.item()
             print("Training Loss: {:.3f}".format(train_loss/len(self.data_loaders['train'])), end='.. ')
-            # Validate
-            model.eval()
-            torch.no_grad()
-            for images, labels in self.data_loaders['valid']:
-                images, labels = images.to(device), labels.to(device)
-                outputs = model.forward(images)
-                test_loss += criterion(outputs, labels).item()
-                ps = torch.exp(outputs)
-                equality = (labels.data == ps.max(dim=1)[1])
-                accuracy += equality.type(torch.FloatTensor).mean()
+            # Test
+            self.model.eval()
+            with torch.no_grad():
+                for images, labels in self.data_loaders['test']:
+                    images, labels = images.to(device), labels.to(device)
+                    outputs = self.model.forward(images)
+                    test_loss += criterion(outputs, labels).item()
+                    ps = torch.exp(outputs)
+                    equality = (labels.data == ps.max(dim=1)[1])
+                    accuracy += equality.type(torch.FloatTensor).mean()
 
             print(
                 "Test Loss: {:.3f}.. ".format(
@@ -224,13 +238,12 @@ class Model:
     
     def validate(self):
         total_match, total = 0, 0
-        model = self.model # shorthand
-        model.eval()
+        self.model.eval()
         torch.no_grad()
         device = self.device # shorthand
         for images, labels in self.data_loaders['valid']:
             images, labels = images.to(device), labels.to(device)
-            outputs = model.forward(images)
+            outputs = self.model.forward(images)
             _, prediction = torch.max(outputs.data, 1)
             total_match += (labels == prediction).sum().item()
             total += labels.size(0)
